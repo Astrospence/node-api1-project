5e4f854d1cb197c38097698f84b013b32e401c7b
Astrospence <illustrator.wood@gmail.com>
2022-01-11 14:36:22 -0700

    all autotests passing

--- api/server.js
+++ api/server.js
@@ -57,4 +57,21 @@ server.delete('/api/users/:id', async (req, res) => {
     }
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        const updatedUser = await Users.update(id, {name, bio})
+        if (!updatedUser) {
+            res.status(404).json({ message: 'The user with the specified ID does not exist' })
+        } else if (!name || !bio) {
+            res.status(400).json({ message: 'Please provide name and bio for the user' })
+        } else {
+            res.status(200).json(updatedUser)
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'The user information could not be modified' })
+    }
+})
+
 module.exports = server // EXPORT YOUR SERVER instead of {}
