242249fb98799ade11f4a51677fe35bafde18fd7
Astrospence <illustrator.wood@gmail.com>
2022-01-11 14:02:02 -0700

    autotests passing for post and 1st get endpoints

--- api/server.js
+++ api/server.js
@@ -10,7 +10,20 @@ server.post('/api/users', async (req, res) => {
     try {
         const { name, bio } = req.body
         const newUser = await Users.insert({ name, bio })
-        res.status(201).json(newUser)
+        if ( !name || !bio ) {
+            res.status(400).json({ message: 'Please provide name and bio for the user' })
+        } else {
+            res.status(201).json(newUser)
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const getUsers = await Users.find()
+        res.status(200).json(getUsers)
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
